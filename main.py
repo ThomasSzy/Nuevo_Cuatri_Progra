88ee8297059aa70856da040d491f14fb807da47c
Thomas Szymuda <thomasszy.tp@gmai.com>
2023-09-24 17:38:37 -0300

    colores

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Chau mundo")
+import pygame
+import sys
+
+pygame.init()  # --> Conectamos Pygame
+
+
+screen = pygame.display.set_mode((800, 600))
+
+
+screen.fill((0, 0, 0))  # --> Red , Green, Blue
+
+
+while True:
+    for event in pygame.event.get():  # -->Nos muestra los eventos que se realicen
+        if event.type == pygame.QUIT:
+            pygame.quit()  # -->Desconectamos pygame
+            sys.exit()  # --> Cerramos programa
+
+    pygame.display.flip()
