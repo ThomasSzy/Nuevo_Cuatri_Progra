c8db233ab031e971eb84a4a47946d318a1313b04
Thomas Szymuda <thomasszy.tp@gmai.com>
2023-09-24 22:37:10 -0300

    Rectangulos_Figuras_Colores

---
+++ clase_7_Colores/config_colores.py
@@ -0,0 +1,22 @@
+
+# Pantalla
+width = 800
+height = 600
+size_screen = (width,height)
+
+# Tiempo
+FPS = 60
+
+
+
+# Colors
+red = (255, 0 , 0)
+green = (0, 255 , 0)
+blue = (0, 0 , 255)
+white = (255, 255, 255)
+black = (0, 0 , 0)
+cyan = (0, 255, 255)
+magenta = (255, 0 , 255)
+yellow  = (255, 255 , 0)
+custom  = (255, 174 , 174)
+
